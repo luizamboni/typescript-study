fa118937d810a601a04f67ab3b310a9f4c3a3151
Luiz Zamboni <luizamboni2002@hotmail.com>
2020-10-26 00:50:43 -0300

    wip: interfaces

--- 2-interfaces/index.js
+++ 2-interfaces/index.js
@@ -1,16 +1,32 @@
 // implicit interface in method signature
 function a(arg) {
-    console.log(arg.label);
+    console.log('a()', arg.label);
 }
 a({ label: '123 ' });
 function b(arg) {
-    console.log(arg.label);
+    console.log('b()', arg.label);
 }
 b({ label: '345' });
 function c(arg) {
-    console.log(arg.label, arg.complement);
+    console.log('c()', arg.label, arg.complement);
 }
 c({ label: '345' });
 c({ label: '345', complement: '232' });
 // note: interface is a minimum of structure that need match
+// note 2: need to be 'casted' becouse have a extra porpertie
 c({ label: '345', complement: '232', otherProp: 'bibliblib' });
+function d(arg) {
+    console.log('d()', arg.label, arg.complement);
+}
+d({ label: '345', complement: '232', otherProp: 'bibliblib' });
+var imutable = {
+    a: "abc",
+    b: "def"
+};
+// it product a compile error
+// imutable.b = "erasdfasdfa";
+console.log(imutable);
+var arr = [1, 2, 5, 7, 8];
+// it product a compile error
+// arr[0] = 4;
+console.log(arr);
--- 2-interfaces/index.ts
+++ 2-interfaces/index.ts
@@ -1,7 +1,7 @@
 
 // implicit interface in method signature
 function a(arg: { label: string }) {
-    console.log(arg.label);
+    console.log('a()', arg.label);
 }
 
 a({ label: '123 '})
@@ -14,7 +14,7 @@ interface Arg {
 }
 
 function b(arg: Arg) {
-    console.log(arg.label);
+    console.log('b()', arg.label);
 }
 
 b({ label: '345' })
@@ -27,7 +27,7 @@ interface ArgToC {
 }
 
 function c(arg: ArgToC) {
-    console.log(arg.label, arg.complement)
+    console.log('c()',arg.label, arg.complement)
 }
 
 c({ label: '345' })
@@ -35,4 +35,52 @@ c({ label: '345', complement: '232' })
 
 
 // note: interface is a minimum of structure that need match
+// note 2: need to be 'casted' becouse have a extra porpertie
 c({ label: '345', complement: '232', otherProp: 'bibliblib' } as ArgToC)
+
+// to accept extra property
+interface ArgToD {
+    label: string;
+    complement?: string;
+    [propName: string]: any;
+}
+
+function d(arg: ArgToD) {
+    console.log('d()',arg.label, arg.complement)
+}
+
+d({ label: '345', complement: '232', otherProp: 'bibliblib' })
+
+// readyonly
+interface ImutableExample {
+    readonly a: string;
+    readonly b: string;
+}
+
+const imutable = {
+    a: "abc",
+    b: "def",
+} as ImutableExample;
+
+// it product a compile error
+// imutable.b = "erasdfasdfa";
+
+console.log(imutable);
+
+const arr : ReadonlyArray<number> = [ 1, 2, 5, 7, 8 ];
+
+// it product a compile error
+// arr[0] = 4;
+
+console.log(arr);
+
+// type function
+interface TypeFuncion {
+    (a: string, b: number ) : boolean
+}
+
+
+const typedFunction : TypeFuncion = (a1: string, b2: number) => true;
+
+// fail
+// const typedFunction : TypeFuncion = (a1: number, b2: number) => true;
